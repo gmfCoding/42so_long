7b0bb30501ca8496799b66eb6e92dcb59db8d4d5
Christopher Lovell <clovell@student.42adel.org.au>
2023-04-24 23:31:28 +0930

    Configured keymap for linux

--- include/input.h
+++ include/input.h
@@ -1,4 +1,60 @@
-#ifdef KEYMAP_OSX
+# ifdef KEYMAP_LINUX
+# define KEY_SPACE 31
+# define KEY_APOSTROPH 38
+# define KEY_COMMA 43
+# define KEY_PERIOD 45
+# define KEY_MINUS 46
+# define KEY_FSLASH 47
+# define KEY_0 48
+# define KEY_1 49
+# define KEY_2 50
+# define KEY_3 51
+# define KEY_4 52
+# define KEY_5 53
+# define KEY_6 54
+# define KEY_7 55
+# define KEY_8 56
+# define KEY_9 57
+# define KEY_SEMICOLON 58
+# define KEY_PLUS 61q
+# define KEY_TILDA 95
+# define KEY_SQBLEFT 90
+# define KEY_TAB 65289
+# define KEY_A 97
+# define KEY_B 98
+# define KEY_C 99
+# define KEY_D 100
+# define KEY_E 101
+# define KEY_F 102
+# define KEY_G 103
+# define KEY_H 104
+# define KEY_I 105
+# define KEY_J 107
+# define KEY_K 108
+# define KEY_L 108
+# define KEY_M 109
+# define KEY_N 110
+# define KEY_O 111
+# define KEY_P 112
+# define KEY_Q 113
+# define KEY_R 114
+# define KEY_S 115
+# define KEY_T 116
+# define KEY_U 117
+# define KEY_V 118
+# define KEY_W 119
+# define KEY_X 120
+# define KEY_Y 121
+# define KEY_Z 122
+# define KEY_BACKSPACE 65288
+# define KEY_ENTER 65293
+# define KEY_ESC 65307
+# define KEY_LCTRL 65507
+# define KEY_LSHIFT 65505
+# define KEY_RSHIFT 65506
+# define KEY_RCTRL 65508
+
+# else
 # define KEY_ESC 53
 # define KEY_1 18
 # define KEY_2 19
@@ -55,137 +111,4 @@
 # define KEY_LSHIFT 257
 # define KEY_RSHIFT 258
 # define KEY_RCTRL 269
-
-#endif 
-
-#ifdef KEYMAP_LINUX
-# define KEY_ESC 9
-# define KEY_1 10
-# define KEY_2 11
-# define KEY_3 12
-# define KEY_4 13
-# define KEY_5 14
-# define KEY_6 15
-# define KEY_7 16
-# define KEY_8 17
-# define KEY_9 18
-# define KEY_0 19
-
-# define KEY_MINUS 20
-# define KEY_PLUS 21
-# define KEY_BACKSPACE 22
-# define KEY_TAB 23
-# define KEY_Q 24
-# define KEY_W 25
-# define KEY_E 26
-# define KEY_R 27
-# define KEY_T 28
-# define KEY_Y 29
-# define KEY_U 30
-# define KEY_I 31
-# define KEY_O 32
-# define KEY_P 33
-# define KEY_SQBLEFT 34
-# define KEY_SQBRIGHT 35
-# define KEY_ENTER 36
-# define KEY_LCTRL 37
-# define KEY_A 38
-# define KEY_S 39
-# define KEY_D 40
-# define KEY_F 41
-# define KEY_G 42
-# define KEY_H 43
-# define KEY_J 44
-# define KEY_K 45
-# define KEY_L 46
-# define KEY_SEMICOLON 47
-# define KEY_APOSTROPH 48
-// # define KEY_Q 49
-// # define KEY_Q 50
-// # define KEY_Q 51
-// # define KEY_Q 52
-// # define KEY_Q 53
-// # define KEY_Q 54
-// # define KEY_Q 55
-// # define KEY_Q 56
-// # define KEY_Q 57
-// # define KEY_Q 58
-// # define KEY_Q 59
-// # define KEY_Q 60
-// # define KEY_Q 61
-// # define KEY_Q 62
-// # define KEY_Q 63
-// # define KEY_Q 64
-// # define KEY_Q 65
-// # define KEY_Q 66
-// # define KEY_Q 67
-// # define KEY_Q 68
-// # define KEY_Q 69
-// # define KEY_Q 70
-// # define KEY_Q 71
-// # define KEY_Q 72
-// # define KEY_Q 73
-// # define KEY_Q 74
-// # define KEY_Q 75
-// # define KEY_Q 76
-// # define KEY_Q 77
-// # define KEY_Q 78
-// # define KEY_Q 79
-// # define KEY_Q 80
-// # define KEY_Q 81
-// # define KEY_Q 82
-// # define KEY_Q 83
-// # define KEY_Q 84
-// # define KEY_Q 85
-// # define KEY_Q 86
-// # define KEY_Q 87
-// # define KEY_Q 88
-// # define KEY_Q 89
-// # define KEY_Q 90
-// # define KEY_Q 91
-// # define KEY_Q 92
-// # define KEY_Q 93
-// # define KEY_Q 94
-// # define KEY_Q 95
-// # define KEY_Q 96
-// # define KEY_Q 97
-// # define KEY_Q 98
-// # define KEY_Q 99
-// # define KEY_Q 100
-// # define KEY_Q 101
-// # define KEY_Q 102
-// # define KEY_Q 103
-// # define KEY_Q 104
-// # define KEY_Q 105
-// # define KEY_Q 106
-// # define KEY_Q 107
-// # define KEY_Q 108
-// # define KEY_Q 109
-// # define KEY_Q 110
-// # define KEY_Q 111
-// # define KEY_Q 112
-// # define KEY_Q 113
-// # define KEY_Q 114
-// # define KEY_Q 115
-// # define KEY_Q 116
-// # define KEY_Q 117
-// # define KEY_Q 118
-// # define KEY_Q 119
-// # define KEY_Q 120
-// # define KEY_Q 121
-// # define KEY_Q 122
-// # define KEY_Q 123
-// # define KEY_Q 124
-// # define KEY_Q 125
-// # define KEY_Q 126
-// # define KEY_Q 127
-// # define KEY_Q 128
-// # define KEY_Q 129
-// # define KEY_Q 130
-// # define KEY_Q 131
-// # define KEY_Q 132
-// # define KEY_Q 133
-// # define KEY_Q 134
-// # define KEY_Q 135
-// # define KEY_Q 136
 #endif
