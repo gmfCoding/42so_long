95d37a63091fe87355c1cf3fd9ca563340ec2c8e
Christopher Lovell <clovell@F3C014.42adel.org.au>
2023-05-17 21:58:18 +0930

    fixed error messages, added new maps

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: clovell <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 12:02:06 by clovell           #+#    #+#             */
-/*   Updated: 2023/05/11 13:09:38 by clovell          ###   ########.fr       */
+/*   Updated: 2023/05/17 21:47:59 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -17,12 +17,14 @@ void	print_errors(t_error error)
 {
 	if (error == E_NONE)
 		return ;
+	else
+		ft_printf("Error\n");
 	if (error & E_EXIT)
-		ft_printf("No exit tile exists, cannot load map!\n");
+		ft_printf("Issue occured with exit tiles, 1 and only 1 must exist!\n");
+	if (error & E_POS)
+		ft_printf("Issue occured with start tile, 1 and only 1 must exist!\n");
 	if (error & E_COLL)
 		ft_printf("No collectables exists, cannot load map!\n");
-	if (error & E_POS)
-		ft_printf("No starting position exists, cannot load map!\n");
 	if (error & E_BOUND)
 		ft_printf("Map leaks into the void, cannot load map!\n");
 	if (error & E_FILE)
